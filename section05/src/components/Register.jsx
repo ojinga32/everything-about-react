ddcdf3f047fef5bdd2fa8d0dafd3d6bb96131fb3
eojin <ojinga32@gmail.com>
2025-11-26 15:15:10 +0900

    중복코드 삭제

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -2,25 +2,18 @@ import { useState } from "react";
 
 function Register() {
     
-    const [name, setName] = useState("이름")
-    const [birth, setBirth] = useState("")
-    const [country, setCountry] = useState("")
-    const [bio, setBio] = useState("")
+    const [input, setInput] = useState({
+        name : "",
+        birth : "",
+        country : "",
+        bio : "",
+    })
 
-    const onChangeName = (e) => {
-        setName(e.target.value)
-    }
-
-    const onChangeBirth = (e) => {
-        setBirth(e.target.value)
-    }
-
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
-
-    const onChangeBio = (e) => {
-        setBio(e.target.value)
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        })
     }
     
     return ( 
@@ -28,21 +21,24 @@ function Register() {
 
             <div>
                 <input 
-                  value={name} 
-                    onChange={onChangeName} 
+                    name = "name"
+                    value={input.name} 
+                    onChange={onChange} 
                     placeholder={"이름"}
                 />
             </div>
 
             <div>
                 <input 
+                    name = "birth"
                     type="date"
-                    onChange={onChangeBirth}
+                    value={input.birth}
+                    onChange={onChange}
                 />
             </div>
 
             <div>
-                <select value={country} onChange={onChangeCountry}>
+                <select name = "country" value={input.country} onChange={onChange}>
                     <option value=""></option>
                     <option value="kr">한국</option>
                     <option value="us">미국</option>
@@ -51,7 +47,7 @@ function Register() {
             </div>
 
             <div>
-                <textarea value={bio} onChange={onChangeBio} />
+                <textarea name = "bio" value={input.bio} onChange={onChange} />
             </div>
 
         </div>
