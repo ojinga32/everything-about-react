68e4797ab80932ba43e4bd21a94753cff73dc83e
eojin <ojinga32@gmail.com>
2025-12-07 00:00:15 +0900

    Todo.css

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,7 +4,9 @@ import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/get-emotion-image'
+import Button from "./components/Button"
+import Header from "./components/Header"
+
 
 
 
@@ -19,28 +21,29 @@ function App() {
   return (
     <>
 
-    <div>
-      <img src={getEmotionImage(1)} />
-      <img src={getEmotionImage(2)} />
-      <img src={getEmotionImage(3)} />
-      <img src={getEmotionImage(4)} />
-      <img src={getEmotionImage(5)} />
-    </div>
+    <Header title={"Header"} leftChild={<Button text={"Left"}/>} rightChild={<Button text={"Right"}/>}/>
 
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>
-        New 페이지로 이동
-      </button>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound /> } /> 
-      </Routes>
+    <Button text={"123"} type={"DEFAULT"} onClick={() => {
+      console.log("123번 클릭")
+    }}
+    />
+  
+    <Button text={"123"} type={"POSITIVE"} onClick={() => {
+      console.log("123번 클릭")
+    }}
+    />
+  
+      <Button text={"123"} type={"NEGATIVE"} onClick={() => {
+      console.log("123번 클릭")
+    }}
+    />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary/:id" element={<Diary />} />
+      <Route path="*" element={<Notfound /> } /> 
+    </Routes>
     </>
 
   )
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useSearchParams } from "react-router-dom";
 
 function Home() {
     const [ params, setParams ] = useSearchParams();
-    console.log(params.get("value"))
+
 
     return ( 
         <div>Home</div>
